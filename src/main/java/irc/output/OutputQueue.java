67cb6ae403582434ce4187503acd4f73ae6691c6
Goldenglue <ember.traveller@gmail.com>
2017-09-14 14:47:56 +0700

    i don't remember

--- src/main/java/irc/output/OutputQueue.java
+++ src/main/java/irc/output/OutputQueue.java
@@ -8,34 +8,36 @@ import java.util.Queue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class OutputQueue {
     private final DoggyBot bot;
     private final Queue<String> queue = new LinkedList<>();
+    private AtomicInteger queueSize = new AtomicInteger();
     private final int delay;
     private final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-    private final Runnable taskRunnable;
     private long lastSent;
 
+    private final Runnable queuePolling = () -> {
+        if (queue.size() > 0) {
+            queueSize.decrementAndGet();
+            lastSent = Instant.now().toEpochMilli();
+            sendNow(queue.poll());
+        }
+    };
 
     public OutputQueue(DoggyBot bot) {
         this.bot = bot;
         this.delay = bot.getConfig().getDelay() * 1000;
-
-        taskRunnable = () -> {
-            if (queue.size() > 0) {
-                lastSent = Instant.now().toEpochMilli();
-                bot.sendLineToServer(queue.poll());
-            }
-        };
-        service.scheduleAtFixedRate(taskRunnable, 0, bot.getConfig().getDelay(), TimeUnit.MILLISECONDS);
+        service.scheduleAtFixedRate(queuePolling, 0, bot.getConfig().getDelay(), TimeUnit.MILLISECONDS);
     }
 
     public void send(String line) {
-        if (queue.size() == 0 && (Instant.now().toEpochMilli() - lastSent) < delay) {
+        if (queueSize.get() == 0 && (Instant.now().toEpochMilli() - lastSent) < delay) {
             lastSent = Instant.now().toEpochMilli();
             sendNow(line);
-        } else if (queue.size() < 10) {
+        } else if (queueSize.get() < 10) {
+            queueSize.incrementAndGet();
             queue.offer(line);
         }
     }
--- src/main/java/twitch/Doggy.java
+++ src/main/java/twitch/Doggy.java
@@ -3,7 +3,7 @@ package twitch;
 import twitch.chat.TwitchChat;
 
 
-//TODO create a database for all this thing so that commands are saved by channel and stuff
+//TODO create a database for all this thing so that commands are saved by channel and stuff (maybe actually user Hibernate?)
 //TODO maybe also create special users commands!
 //TODO move to maven so modules actually work
 public class Doggy {
